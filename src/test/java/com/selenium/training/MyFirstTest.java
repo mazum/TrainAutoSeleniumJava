0ce748cf624743dd5cdf2631f8f453fa8b92a7b4
Abhishek Mazumder <mazumder.abhishek@gmail.com>
2017-08-28 21:25:43 +1000

    Send Key and Click along with Thread.

--- src/test/java/com/selenium/training/MyFirstTest.java
+++ src/test/java/com/selenium/training/MyFirstTest.java
@@ -1,17 +1,20 @@
 package com.selenium.training;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class MyFirstTest {
     @Test
-    public void  startWebDriver(){
+    public void  startWebDriver() throws InterruptedException {
         WebDriver driver=new ChromeDriver();
         driver.navigate().to("http://www.google.com");
         Assert.assertTrue(driver.getTitle().startsWith("Google"), "Title should start with Google");
+        driver.findElement(By.name("q")).sendKeys("Selenium");
+        driver.findElement(By.name("btnK")).click();
+        Thread.sleep(2000);
         driver.close();
         driver.quit();
     }
