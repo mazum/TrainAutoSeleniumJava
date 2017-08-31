323b1ce7817a27ba343b9364cc7e4e3a193691f5
Abhishek Mazumder <mazumder.abhishek@gmail.com>
2017-08-31 22:27:12 +1000

    Firefox support with Selenium 2.53.1 and Firefox 45.

--- src/test/java/com/selenium/training/MyFirstTest.java
+++ src/test/java/com/selenium/training/MyFirstTest.java
@@ -3,13 +3,14 @@ package com.selenium.training;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class MyFirstTest {
     @Test
     public void  startWebDriver() throws InterruptedException {
-        WebDriver driver=new ChromeDriver();
+        WebDriver driver=new FirefoxDriver();
         driver.navigate().to("http://www.google.com");
         Assert.assertTrue(driver.getTitle().startsWith("Google"), "Title should start with Google");
         driver.findElement(By.id("lst-ib")).sendKeys("Selenium");
