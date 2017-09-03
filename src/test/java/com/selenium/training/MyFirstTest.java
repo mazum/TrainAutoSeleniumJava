496612fa977b783717873c3f3ef4eac0d483ba6e
Abhishek Mazumder <mazumder.abhishek@gmail.com>
2017-09-04 09:20:59 +1000

    First Commit

--- src/test/java/com/selenium/training/MyFirstTest.java
+++ src/test/java/com/selenium/training/MyFirstTest.java
@@ -9,25 +9,19 @@ import org.testng.annotations.Test;
 
 public class MyFirstTest {
     @Test
-    public void  startWebDriver() throws InterruptedException {
-        WebDriver driver=new FirefoxDriver();
-        driver.navigate().to("http://www.google.com");
-        Assert.assertTrue(driver.getTitle().startsWith("Google"), "Title should start with Google");
-        driver.findElement(By.id("lst-ib")).sendKeys("Selenium");
-        driver.findElement(By.name("btnK")).click();
-        driver.findElement(By.className("g")).click();
+    public void googleSearch() throws InterruptedException {
+        WebDriver driver=driverFactory("Chrome");
+        driver.navigate().to("https://www.google.co.nz/");
         Thread.sleep(2000);
         driver.close();
         driver.quit();
     }
 
-    @Test
-    public void loginToGmail(){
-        WebDriver driver=new ChromeDriver();
-        driver.navigate().to("https://www.gmail.com");
-        driver.findElement(By.id("identifierId")).sendKeys("theknowledgeacademy");
-        driver.findElement(By.id("identifierNext")).click();
-        driver.close();
-        driver.quit();
+    private WebDriver driverFactory(String browser){
+        if (browser.equals("Chrome"))
+            return new ChromeDriver();
+        if (browser.equals("FF"))
+            return new FirefoxDriver();
+        throw new NotImplementedException();
     }
 }
