ff97a76943403719cb925064ea575bb61708281f
Abhishek Mazumder <mazumder.abhishek@gmail.com>
2017-09-05 07:15:15 +1000

    Wait and Synchronization related codes.

--- src/test/java/com/selenium/training/MyFirstTest.java
+++ src/test/java/com/selenium/training/MyFirstTest.java
@@ -4,16 +4,19 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
-import org.testng.annotations.AfterTest;
-import org.testng.annotations.BeforeTest;
-import org.testng.annotations.Test;
+import org.testng.annotations.*;
 import sun.reflect.generics.reflectiveObjects.NotImplementedException;
+
+import java.util.concurrent.TimeUnit;
 
 public class MyFirstTest {
     private WebDriver driver;
 
-    @BeforeTest
+    @BeforeMethod
     public void beforeTest()
     {
         driver=driverFactory("Chrome");
@@ -21,11 +24,25 @@ public class MyFirstTest {
 
     @Test
     public void googleSearch() throws InterruptedException {
-        driver.navigate().to("https://www.google.co.nz/");
+        driver.navigate().to("https://www.google.com/");
+        Assert.assertEquals(driver.getTitle(),"Google");
+        Assert.assertTrue(driver.getCurrentUrl().startsWith("https://www.google.co"),"URL is not in google domain");
+        System.out.println(driver.findElement(By.className("logo-subtext")).getText());
+        Assert.assertEquals(driver.findElement(By.className("logo-subtext")).getText(),"New Zealand");
+        Thread.sleep(2000);
+        WebDriverWait wait=new WebDriverWait(driver,10);
+        wait.until(ExpectedConditions.elementToBeClickable(By.name("btnK"))).click();
+    }
+
+    @Test
+    public void gmailLogin() throws InterruptedException {
+        driver.navigate().to("https://www.gmail.com/");
+        driver.findElement(By.id("identifierId")).sendKeys("selenium");
+        driver.findElement(By.id("identifierNext")).click();
         Thread.sleep(2000);
     }
 
-    @AfterTest
+    @AfterMethod
     public void afterTest()
     {
         driver.close();
