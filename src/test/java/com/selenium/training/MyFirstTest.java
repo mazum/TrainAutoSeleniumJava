d43a290aaafd0e20d615c5c6979aa19bd42085ad
Abhishek Mazumder <mazumder.abhishek@gmail.com>
2017-09-05 22:16:02 +1000

    JavaScript Executor code.

--- src/test/java/com/selenium/training/MyFirstTest.java
+++ src/test/java/com/selenium/training/MyFirstTest.java
@@ -1,6 +1,7 @@
 package com.selenium.training;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
@@ -16,14 +17,9 @@ import java.util.concurrent.TimeUnit;
 public class MyFirstTest {
     private WebDriver driver;
 
-    @BeforeMethod
-    public void beforeTest()
-    {
-        driver=driverFactory("Chrome");
-    }
-
     @Test
     public void googleSearch() throws InterruptedException {
+        driver=driverFactory("Chrome");
         driver.navigate().to("https://www.google.com/");
         Assert.assertEquals(driver.getTitle(),"Google");
         Assert.assertTrue(driver.getCurrentUrl().startsWith("https://www.google.co"),"URL is not in google domain");
@@ -32,22 +28,42 @@ public class MyFirstTest {
         Thread.sleep(2000);
         WebDriverWait wait=new WebDriverWait(driver,10);
         wait.until(ExpectedConditions.elementToBeClickable(By.name("btnK"))).click();
+        driver.close();
+        //driver.quit();
     }
 
     @Test
     public void gmailLogin() throws InterruptedException {
+        driver=driverFactory("Chrome");
         driver.navigate().to("https://www.gmail.com/");
         driver.findElement(By.id("identifierId")).sendKeys("selenium");
         driver.findElement(By.id("identifierNext")).click();
         Thread.sleep(2000);
+        driver.close();
+        //driver.quit();
     }
 
-    @AfterMethod
-    public void afterTest()
-    {
+    @Test
+    public void scrollUp() throws InterruptedException {
+        driver=driverFactory("Chrome");
+        driver.navigate().to("https://www.google.com/");
+        driver.findElement(By.name("q")).sendKeys("Selenium");
+        driver.findElement(By.name("btnK")).click();
+
+        Thread.sleep(5000);
+
+        JavascriptExecutor js = (JavascriptExecutor)driver;
+        //Vertical scroll - down by 150  pixels
+        js.executeScript("window.scrollBy(0,150)");
+
+        Thread.sleep(5000);
+
         driver.close();
-        driver.quit();
+        //driver.quit();
+
+        //driver.findElement(By.id("pnnext")).click();
     }
+
 
     private WebDriver driverFactory(String browser){
         if (browser.equals("Chrome"))
